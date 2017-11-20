bc50b99e504f4a9e16b4bb254cabb0b72e354b3e
Robert Rice <rarice4@gmail.com>
2017-11-19 21:51:39 -0700

    mongo session storage

--- src/app.js
+++ src/app.js
@@ -2,12 +2,31 @@
 
 var express = require('express');
 var router = require('./api');
-var parser = require("body-parser");
+var parser = require('body-parser');
+var authSecret = require('authSecret');
 var app = express();
 
 require('./database');
 require('./seed');
 app.use("/",express.static('public'));
+
+var db = mongoose.connection;
+db.on('error', console.error.bind(console,'connection error: ', ));
+//use sessions for tracking logins
+app.use(session({
+  secret: authSecret.secret,
+  resave: true,
+  saveUninitialized: false,
+  store: new MongoStore({
+    mongooseConnection: db
+  })
+}));
+
+// make use Id available in templates
+app.use(function(req,res,next){
+  res.locals.currentUser = req.session.userId;
+  next();
+})
 
 
 app.use(parser.json());
